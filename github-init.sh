2a857a573e469cd93afd66361eba1c3de3b42b8c
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-06 15:00:59 -0500

    Actually process arguments and add switch for init

--- github-init.sh
+++ github-init.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
-NAME="testing123"
-DESCRIPTION="This is my test repo for the api"
-USERNAME="radicalzephyr"
+TEMP=$(getopt -o 'i' --long "init" -n $(basename $0) -- "$@")
 
-APITOKEN=""
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 
-PAYLOAD="{\"name\": \"$NAME\",\"description\": \"$DESCRIPTION\",\"homepage\": \"https://github.com/$USERNAME/$NAME\",\"auto_init\": true}"
+eval set -- "$TEMP"
+
+while true; do
+    case "$1" in
+        -i | --init ) INIT=true; shift 1 ;;
+        -- ) shift; break ;;
+    esac
+done
+
+NAME="$1"
+DESCRIPTION="$2"
+
+USERNAME="${GH_USERNAME}"
+
+APITOKEN="${GH_API_TOKEN}"
+
+
+PAYLOAD="{\"name\": \"$NAME\",\"description\": \"$DESCRIPTION\",\"homepage\": \"https://github.com/$USERNAME/$NAME\",\"auto_init\": ${INIT-false}}"
 
 if curl -d "${PAYLOAD}" -H "Authorization: token ${APITOKEN}" https://api.github.com/user/repos
 then
